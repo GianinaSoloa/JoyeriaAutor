aaf5e93b45d9981aacaf91e8e3af1510a088c3f6
GianinaSoloa <gianina.soloa@hotmail.com>
2022-08-30 20:02:15 -0300

    últimos cambios

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                 <p>comodecirtelojoyas@gmail.com</p>
             </div>
             <div className="footer__contact">
-                <a href="https://api.whatsapp.com/send?phone=0123456789&text=Hola, Nececito mas informacion sobre" target="_blank" rel="noreferrer"><PhoneIcon className="icon__contact"/></a>
+                <a href="https://api.whatsapp.com/send?phone=+5492216130884&text=Hola!%20Ingres%C3%A9%20a%20la%20web%20de%20C%C3%B3mo%20Dec%C3%ADrtelo%20Joyas,%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre" target="_blank" rel="noreferrer"><PhoneIcon className="icon__contact"/></a>
                 <p>221.613.0884</p>
             </div>
             <div className="footer__contact">
